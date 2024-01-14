b1b1d92844fa9e8dda504e967af1c49541ff42ed
YuriiRogovets <yura.rogovets@gmail.com>
2024-01-14 14:31:28 +0200

    tasck 1

--- lesson-05/module-08/js/app.js
+++ lesson-05/module-08/js/app.js
@@ -5,9 +5,18 @@
  * розкритий текст приховується.Нажимаючи на інші елементи меню, попередньо відкриті елементи не закриваються.
  */
 
+const listEl = document.querySelector(".js-accordion-list")
+listEl.addEventListener("click", event => {
+    if (event.target.nodeName !== "BUTTON") {
+        return
+    }
+    event.target.nextElementSibling.classList.toggle("active");
+    
+})
 //TODO:=========task-02=======Pagination==========
 /**
  * Викоритовуй шаблон пагінації з файлу[pagination.html](./pagination.html) для розробки функціоналу пагінатора.
  * Використовуй поширення подій.Додавай клас`active` на поточну сторінку. Створи допоміжну функцію`updateActivePage`, яка повинна обновляти активний клас.
  * Функція`handleClick`повинна викликатися коли відбувається клік на елементі`pagination`.
  */
+
